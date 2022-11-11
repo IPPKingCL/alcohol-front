56255ef926db426de9091b80e62adab56034e913
hjkwon0814 <hjkwon0814@naver.com>
2022-11-11 09:52:25 +0900

    add resources file

---
+++ alcohol/src/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Login() {
+  return (
+    <div>
+        <input type="text" />
+        <img src="/resources/btn_google_signin_dark_disabled_web.png"></img>
+    </div>
+  );
+}
+
+export default Login;
