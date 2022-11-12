4f3a4492577e0c5889b2c83a7c67f0ccdf7053ce
hjkwon <hjkwon0814@naver.com>
2022-11-12 23:22:02 +0900

    modified form tag

--- alcohol/src/AddInfo.tsx
+++ alcohol/src/AddInfo.tsx
@@ -14,11 +14,12 @@ function AddInfo() {
 
   return (
     <div className='addInfoInputTag'>
+      <h1>추가 정보를 입력해 주세요.</h1> 
       <form className='formAlign' method='post' action='/main'>
-        <input type="text" placeholder='nickname'></input><hr/>
-        <input type="text" placeholder='job'></input><hr/>
-        <input type="text" placeholder='Maximum price'></input><hr/>
-        <input type="text" placeholder='favorite List'></input><hr/>
+        <h3>닉네임 : <input type="text" placeholder='nickname' required></input> </h3><hr/>
+        <h3>직업 : <input type="text" placeholder='job' required></input></h3><hr/>
+        <h3>허용 최대 가격 : <input type="text" placeholder='Maximum price' required></input></h3><hr/>
+        <h3>좋아하는 목록 : <input type="select" placeholder='favorite List' required></input></h3><hr/>
         <input type="submit" value="완료"></input>
       </form>
     </div>
