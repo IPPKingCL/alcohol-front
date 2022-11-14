cf500c359e28ad4a25ff5e782836a1b3dde4b2ab
hjkwon0814 <hjkwon0814@naver.com>
2022-11-14 10:48:05 +0900

    input value interface에 값 넣기

--- alcohol/src/AddInfo.tsx
+++ alcohol/src/AddInfo.tsx
@@ -9,10 +9,10 @@ import internal from 'stream';
 function AddInfo() {
 
   interface UserAddInfo {
-    nickname : string;
-    job : string;
-    MaximumPrice : number;
-    favoriteList : Array<string>;
+    nickname: string;
+    job: string;
+    MaximumPrice: number;
+    favoriteList: Array<string>;
   }
 
   const navigate = useNavigate();
@@ -21,18 +21,80 @@ function AddInfo() {
     navigate("/main");
   }
 
-  const [userAddInfo, setUserAddInfo] = useState<UserAddInfo>();
+  const [userAddInfo, setUserAddInfo] = useState<UserAddInfo>({
+    nickname: '',
+    job: '',
+    MaximumPrice: 0,
+    favoriteList: new Array,
+  });
+
+  const {nickname, job, MaximumPrice, favoriteList} = userAddInfo;
+
+  const onChangeNickname = (e : any) => {
+    const { name, value } = e.target;
+ 
+
+    const nextNickNameInput : UserAddInfo = {
+      ...userAddInfo,
+      [name] : value,
+    }
+    setUserAddInfo(nextNickNameInput);
+    console.log(nextNickNameInput);
+    
+  }
+
+  const onChangeJob = (e : any) => {
+    const { name, value } = e.target;
+
+
+    const nextNickNameInput : UserAddInfo = {
+      ...userAddInfo,
+      [name] : value,
+    }
+    setUserAddInfo(nextNickNameInput);
+    console.log(nextNickNameInput);
+    
+  }
+
+  const onChangeMaximumPrice = (e : any) => {
+    const { name, value } = e.target;
+
+
+    const nextNickNameInput : UserAddInfo = {
+      ...userAddInfo,
+      [name] : value,
+    }
+    setUserAddInfo(nextNickNameInput);
+    console.log(nextNickNameInput);
+    
+  }
+
+  const onChangeFavoriteList = (e : any) => {
+    const { name, value } = e.target;
+
+    
+    console.log("value" + value);
+
+    const nextNickNameInput : UserAddInfo = {
+      ...userAddInfo,
+      [name] : value,
+    }
+    setUserAddInfo(nextNickNameInput);
+    console.log(nextNickNameInput);
+    
+  }
+
 
   function sendAddInfo() {
-    fetch('http://localhost:5000/UserAddInfo' , {
-      method : "POST",
-      headers : {
-        "Content-Type" : "application/json",
+    fetch('http://localhost:5000/UserAddInfo', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
       }),
     }).then(res => {
-      if(res.ok) {
+      if (res.ok) {
         alert("응답완료");
       }
     })
@@ -41,12 +103,12 @@ function AddInfo() {
 
   return (
     <div className='addInfoInputTag'>
-      <h1>추가 정보를 입력해 주세요.</h1> 
+      <h1>추가 정보를 입력해 주세요.</h1>
       <div className='formAlign'>
-        <h3>닉네임 : <input type="text" placeholder='nickname' name='nickname'required></input><button>중복 확인</button></h3><hr/>
-        <h3>직업 : <input type="text" placeholder='job' name='job' required></input></h3><hr/>
-        <h3>허용 최대 가격 : <input type="text" placeholder='Maximum price' name='MaximumPrice' required></input></h3><hr/>
-        <h3>좋아하는 목록 : <select name='list1'></select><select name='list2'></select><select name='list3'></select></h3><hr/>
+        <h3>닉네임 : <input type="text" placeholder='nickname' name='nickname' onChange={onChangeNickname} required></input><button>중복 확인</button></h3><hr />
+        <h3>직업 : <input type="text" placeholder='job' name='job' onChange={onChangeJob} required></input></h3><hr />
+        <h3>허용 최대 가격 : <input type="text" placeholder='Maximum price' name='MaximumPrice' onChange={onChangeMaximumPrice} required></input></h3><hr />
+        <h3>좋아하는 목록 : <select name='favoriteList'></select><select name='favoriteList'></select><select name='favoriteList'></select></h3><hr />
         <input type="submit" value="완료"></input>
       </div>
     </div>
