8745ebc859ba9b17d81ec9db79e0df91f30d92e0
chobkyu <qudqud97@naver.com>
2022-11-23 16:54:47 +0900

    댓글 등록 기능

--- alcohol/src/Board/Comment.tsx
+++ alcohol/src/Board/Comment.tsx
@@ -1,9 +1,38 @@
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { addr } from '../interface/serverAddr';
 
 function Comment(){
+    const [content, setContent] = useState<string>();
+    const {id} = useParams();
+    const onchange = (e:any) => {
+        setContent(e.target.value);
+    }
+    const onclick = () => { 
+        fetch(addr+'/board/insertComment',{
+            method:"POST",
+            headers:{
+                "Content-Type" : "application/json",
+            },
+            body : JSON.stringify({
+                contents : content,
+                nickname : "bk",//가라데이터 임시 사용
+                boardId : id
+            }),
+        }).then((res) => res.json())
+        .then((res) => {
+            if(res.success){
+                alert("등록 성공");
+            }else{
+                alert("등록 실패");
+            }
+        })
+    }
+
     return(
-        <div>
-            hi
+        <div className="comment-box">
+            <input type="text" name="comment" onChange={onchange}></input>
+            <button onClick={onclick}>등록</button>
         </div>
     )
 }
