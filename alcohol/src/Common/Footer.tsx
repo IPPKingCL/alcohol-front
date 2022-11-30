d53b2317e051f31c963cc0c095cf69c596835b23
chobkyu <qudqud97@naver.com>
2022-11-30 09:35:45 +0900

    대충 링크

--- alcohol/src/Common/Footer.tsx
+++ alcohol/src/Common/Footer.tsx
@@ -1,16 +1,27 @@
+import { useNavigate } from 'react-router-dom';
 import '../css/footer.css';
 
 function Footer(){
+    const navigate = useNavigate();
+    const recommend = () => {};//술 추천
+    const mbti = () => {};//오늘의 술
+    const home = () => {navigate('/Main')};
+    const free = () => {navigate('/free')};
+    const myPage = () => {};
+
     return(
         <div>
            
-                <div id="footer">
-                    <li className="bar_menu"></li>
-                    <li className="bar_menu"></li>
-                    <li className="bar_menu"></li>
-                    <li className="bar_menu"></li>
-                    <li className="bar_menu"></li>
+            <div id="footer">
+                <div className='menu'>
+                    <li onClick={recommend} className="bar_menu">오늘의 술</li>
+                    <li onClick={mbti} className="bar_menu">술 MBTI</li>
+                    <li onClick={home} className="bar_menu">홈</li>
+                    <li onClick={free} className="bar_menu">게시판</li>
+                    <li onClick={myPage} className="bar_menu">마이페이지</li>
                 </div>
+                
+            </div>
 
         </div>
     )
