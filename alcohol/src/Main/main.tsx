f147bf093936994d71a40e70bf2248f995fdc22d
hjkwon0814 <hjkwon0814@naver.com>
2022-12-01 09:13:16 +0900

    main push

---
+++ alcohol/src/Main/main.tsx
@@ -0,0 +1,16 @@
+import '../css/App.css';
+import '../css/Login.css';
+
+function Main() {
+
+   
+    return (
+
+       <div>
+            <h1>메인 페이지 입니다.</h1>
+       </div>
+    );
+
+}
+
+export default Main;
