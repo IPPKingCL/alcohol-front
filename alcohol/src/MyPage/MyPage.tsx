9f447143d6c9a9336aa833ee14d68eda24b08f49
chobkyu <qudqud97@naver.com>
2022-12-02 18:16:24 +0900

    대강 퍼블리싱 및 추천기능 시작

--- alcohol/src/MyPage/MyPage.tsx
+++ alcohol/src/MyPage/MyPage.tsx
@@ -1,27 +1,39 @@
+import '../css/MyPage.css';
+
 function MyPage(){
     return(
         <div>
-            <div>
+            <div className='mypage'>
                 <h3>(세션)님의 마이페이지</h3>
             </div>
             
             <hr></hr>
             <div>
-                <img src="https://ifh.cc/g/QCO7Gm.png"/>
-                <p>이름</p>
-                <span>안녕하세요 등 소개글 작성</span>
+                <img className="myImg" src="https://ifh.cc/g/QCO7Gm.png"/>
+                <div >
+                    <p className='myName'>이름</p>
+                    <span className='myHi'>안녕하세요 등 소개글 작성</span>{/*css 변경 예정*/}
+                </div>
+                
             </div>
 
-            <div>
+            <div className='myDrink'>
                 <p>좋아하는 술 목록</p>{/*이 부분 컴포넌트 분리해서 map으로 뿌릴 예정입니다*/}
-                <span>1 </span>
-                <input type="text" value={"잭 다니엘"}/>
-
-                <span>2 </span>
-                <input type="text" value={"스미노프 레드"}/>
-
-                <span>3 </span>
-                <input type="text" value={"메이커스 마커스"}/>
+                <div>
+                    <span>1 </span>
+                    <input type="text" value={"잭 다니엘"}/>
+                </div>
+                
+                <div>
+                    <span>2 </span>
+                    <input type="text" value={"스미노프 레드"}/>
+                </div>
+                
+                <div>
+                    <span>3 </span>
+                    <input type="text" value={"메이커스 마커스"}/>
+                </div>
+               
             </div>
 
 
