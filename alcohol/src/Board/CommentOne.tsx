ef11f8a1f189e047118345d6f3b764d20d02b9a2
chobkyu <qudqud97@naver.com>
2022-12-03 09:38:55 +0900

    댓글 기능(이슈 있음)

--- alcohol/src/Board/CommentOne.tsx
+++ alcohol/src/Board/CommentOne.tsx
@@ -1,10 +1,30 @@
+import { addr } from "../interface/serverAddr";
 
 
 function CommentOne(prop:any){
-    
+    const onclick = async () => {
+        //아이디 비교하는 과정 필요
+        
+        fetch(addr + '/board/deleteComment/'+prop.data.id,{
+            method:"GET",
+            headers:{
+                "Content-Type":"application/json",
+            },
+            
+        }).then((res)=>res.json())
+        .then((res) => {
+            if(res.success){
+                alert('댓글이 삭제되었습니다');
+               
+            }else{
+                console.log("댓글 삭제 과정 중 에러발생");
+                alert("삭제 과정 중 에러발생 \n다시 시도해주세요");
+            }
+        });
+    }
     return (
         <div>
-            {prop.data.nickname} : <input type="text" className="comment-input" value={prop.data.contents} disabled/> <span className="comment-span">삭제</span>
+            {prop.data.nickname} : <input type="text" className="comment-input" value={prop.data.contents} disabled/> <span onClick={onclick} className="comment-span">삭제</span>
             <hr></hr>
         </div>
     )
