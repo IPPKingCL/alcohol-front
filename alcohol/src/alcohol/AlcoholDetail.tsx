f4c49b60c2b7037efefea6c45b89372f5947406b
chobkyu <qudqud97@naver.com>
2022-12-11 11:59:50 +0900

    페이지 이동 후 데이터 불러오는데까지

--- alcohol/src/alcohol/AlcoholDetail.tsx
+++ alcohol/src/alcohol/AlcoholDetail.tsx
@@ -1,4 +1,26 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getCookie } from "../Common/Cookies";
+import { addr } from "../interface/serverAddr";
+
 function AlcoholDetail(prop:any){
+    const {id} = useParams();
+    const detail = () => {
+        fetch(addr+'/alcohol/detail/'+id,{
+            method:"GET",
+            headers:{
+                "Authorization":`Bearer ${getCookie('myToken')}`,
+            }
+        }).then((res) => res.json())
+        .then((res)=>{
+            console.log(res)
+        })
+    }
+
+    useEffect(() => {
+        detail()
+    },[]);
+
     return (
         <div>
 
--- alcohol/src/alcohol/AlcoholTable.tsx
+++ alcohol/src/alcohol/AlcoholTable.tsx
@@ -1,9 +1,16 @@
 import { PresignedPost } from "aws-sdk/clients/s3";
+import { useNavigate } from "react-router-dom";
 
 function AlcoholTable(prop:any){
+    const navigate = useNavigate();
+
+    const onclick = () => {
+        navigate('/alcohol/detail/'+prop.data.id);
+    }
+
     return (
         <div className="alchoWrapper"> 
-            <h2 className="board-title">{prop.data.name}</h2>
+            <h2 className="board-title" onClick={onclick}>{prop.data.name}</h2>
             <span className="board-title"> {prop.data.category}</span>
             <img className="alcoholImg" src={prop.data.imgUrl}/>
             <hr></hr>
