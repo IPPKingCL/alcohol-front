9054df80e809d58dc7a5dfbf9274caff9c5e34f9
chobkyu <qudqud97@naver.com>
2022-12-11 16:28:21 +0900

    술 디테일 페이지

--- alcohol/src/alcohol/AlcoholDetail.tsx
+++ alcohol/src/alcohol/AlcoholDetail.tsx
@@ -1,11 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getCookie } from "../Common/Cookies";
+import { alcho } from "../interface/Alcho";
 import { addr } from "../interface/serverAddr";
 
 function AlcoholDetail(prop:any){
     const {id} = useParams();
-    const detail = () => {
+    const [loading, setLoading] = useState<boolean>(true);
+    const [alcoholData,setAlcoholData] = useState<alcho>();
+    const detail = async () => {
         fetch(addr+'/alcohol/detail/'+id,{
             method:"GET",
             headers:{
@@ -14,6 +17,8 @@ function AlcoholDetail(prop:any){
         }).then((res) => res.json())
         .then((res)=>{
             console.log(res)
+            setAlcoholData(res);
+            setLoading(false);
         })
     }
 
@@ -23,7 +28,18 @@ function AlcoholDetail(prop:any){
 
     return (
         <div>
-
+            {loading ? <strong>loading...</strong>:
+            <div>
+                <h2>{alcoholData?.name}</h2>
+                <span>종류 : {alcoholData?.category} </span>
+                <span>당도 : {alcoholData?.sugar} </span>
+                <span>색 : {alcoholData?.color} </span>
+                <span>도수 : {alcoholData?.dosu} </span>
+                <span>가격 : {alcoholData?.price} </span>
+                <img src = {alcoholData?.imgUrl}/>
+            </div>
+                
+            }
         </div>
     )
 
