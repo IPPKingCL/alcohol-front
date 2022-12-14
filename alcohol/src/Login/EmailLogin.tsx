63a2003a760e66773778295fcab7fd0b26aebc35
hjkwon0814 <hjkwon0814@naver.com>
2022-12-14 16:46:10 +0900

    login 추가

---
+++ alcohol/src/Login/EmailLogin.tsx
@@ -0,0 +1,13 @@
+import React, { useState, forwardRef } from 'react';
+import '../css/App.css';
+import '../css/Login.css';
+
+function EmailLogin() {
+
+    return (
+        <div>
+
+
+        </div>
+    );
+}
