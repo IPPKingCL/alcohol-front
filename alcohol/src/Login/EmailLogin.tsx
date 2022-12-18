dad31da83390fb4276e189deacc2a6b4328084e7
hjkwon <hjkwon0814@naver.com>
2022-12-18 20:42:03 +0900

    password 가림처리

--- alcohol/src/Login/EmailLogin.tsx
+++ alcohol/src/Login/EmailLogin.tsx
@@ -1,4 +1,4 @@
-import { Box, FormControlLabel, Switch, TextField } from '@mui/material';
+import { Box, FormControl, FormControlLabel, IconButton, InputAdornment, InputLabel, OutlinedInput, Switch, TextField } from '@mui/material';
 import React, { useState, forwardRef, useEffect } from 'react';
 import '../css/App.css';
 import '../css/Login.css';
@@ -7,8 +7,17 @@ import SendIcon from '@mui/icons-material/Send';
 import { LoadingButton } from '@mui/lab';
 import { addr } from '../Common/serverAddr';
 import { UserEmailLogin } from '../interface/UserEmailLogin';
+import { VisibilityOff, Visibility } from '@mui/icons-material';
 
 function EmailLogin() {
+
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
 
     const [loading, setLoading] = React.useState<boolean>(false);
     const [userEmailPassword, setUserEmailPassword] = React.useState<UserEmailLogin>({
@@ -69,7 +78,7 @@ function EmailLogin() {
                     alert("존재하지 않는 사용자입니다.");
                     setLoading(false);
                 })
-        }else {
+        } else {
             alert('이메일 또는 패스워드를 입력하세요.');
         }
 
@@ -78,10 +87,31 @@ function EmailLogin() {
     return (
         <div>
             <div style={{ textAlign: "center" }}>
-                <h2 style={{ display: "inline-block" }}>Email : &nbsp;</h2><TextField id="outlined-basic" name="email" label="Outlined" variant="outlined" onChange={changeEmail} />
+                <h2 style={{ display: "inline-block" }}>Email : &nbsp;</h2><TextField id="outlined-basic" name="email" label="Email" variant="outlined" onChange={changeEmail} />
             </div>
             <div style={{ textAlign: "center" }}>
-                <h2 style={{ display: "inline-block" }}>Password : &nbsp;</h2><TextField id="outlined-basic" name="password" label="Outlined" variant="outlined" onChange={changePassword} />
+                <h2 style={{ display: "inline-block" }}>Password : &nbsp;</h2>
+                <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-password"
+                        type={showPassword ? 'text' : 'password'}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="Password"
+                        onChange={changePassword}
+                    />
+                </FormControl>
             </div>
             <div style={{ textAlign: 'center' }}>
                 <Box>
