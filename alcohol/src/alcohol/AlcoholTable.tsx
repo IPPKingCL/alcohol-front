f01cf6cc8f07da5ba168e8fbb0b0cf323f8a7ccb
hjkwon0814 <hjkwon0814@naver.com>
2022-12-21 11:17:32 +0900

    grid 추가해서 카드 가운데 정렬하기

--- alcohol/src/alcohol/AlcoholTable.tsx
+++ alcohol/src/alcohol/AlcoholTable.tsx
@@ -19,6 +19,7 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import React, { useEffect, useState } from "react";
 import { addr } from "../Common/serverAddr";
 import { getCookie } from "../Common/Cookies";
+import { Grid } from "@mui/material";
 
 interface ExpandMoreProps extends IconButtonProps {
     expand: boolean;
@@ -27,28 +28,28 @@ interface ExpandMoreProps extends IconButtonProps {
 const ExpandMore = styled((props: ExpandMoreProps) => {
     const { expand, ...other } = props;
     return <IconButton {...other} />;
-  })(({ theme, expand }) => ({
+})(({ theme, expand }) => ({
     transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
     marginLeft: 'auto',
     transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
+        duration: theme.transitions.duration.shortest,
     }),
 }));
-  
 
-function AlcoholTable(prop:any){
+
+function AlcoholTable(prop: any) {
     const navigate = useNavigate();
-    const [color,setColor] = useState<string>(); 
-    
-    useEffect(()=>{
-        if(prop.data.color==='tp'){
+    const [color, setColor] = useState<string>();
+
+    useEffect(() => {
+        if (prop.data.color === 'tp') {
             setColor("Transparency(무색)")
-        }else{
+        } else {
             setColor(prop.data.color);
         }
-    },[]);
+    }, []);
     const onclick = () => {
-        navigate('/alcohol/detail/'+prop.data.id);
+        navigate('/alcohol/detail/' + prop.data.id);
     }
 
     const [expanded, setExpanded] = React.useState(false);
@@ -58,20 +59,20 @@ function AlcoholTable(prop:any){
     };
 
     const like = () => {
-        fetch(addr+'/alcohol/like/'+prop.data.id,{
-            method : "GET",
-            headers:{
+        fetch(addr + '/alcohol/like/' + prop.data.id, {
+            method: "GET",
+            headers: {
                 "Content-Type": "application/json",
                 "Authorization": `Bearer ` + getCookie('myToken'),
             }
         }).then(res => res.json())
-        .then((res) => {
-            if(res.success){
-                alert("추천완료")
-            }else{
-                alert(res.msg);
-            }
-        })
+            .then((res) => {
+                if (res.success) {
+                    alert("추천완료")
+                } else {
+                    alert(res.msg);
+                }
+            })
     }
 
     const cocktail = () => {
@@ -79,78 +80,80 @@ function AlcoholTable(prop:any){
     }
 
     return (
-        <div className="alchoWrapper" > 
-            <Card sx={{ maxWidth: 390 }}>
-                <CardHeader
-                    avatar={
-                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                        <img className="alcoholImg" src={prop.data.imgUrl}/>
-                    </Avatar>
-                    }
-                    action={
-                    <IconButton aria-label="settings">
-                        <MoreVertIcon />
-                    </IconButton>
-                    }
-                    title={prop.data.name}
-                    subheader={prop.data.category}
-                />
-                <CardMedia
-                    component="img"
-                    height="400"
-                    image={prop.data.imgUrl}
-                    alt="Paella dish"
-                    onClick={onclick}
-                />
-                <CardContent>
-                    <Typography variant="body2" color="text.secondary">
-                        
-                    </Typography>
-                </CardContent>
-                <CardActions disableSpacing>
-                    <IconButton aria-label="add to favorites" onClick={like}>
-                        <FavoriteIcon/>
-                    </IconButton>
-                    {/*<IconButton aria-label="share">
+        <div className="alchoWrapper" >
+            <Grid style={{ display:'flex', justifyContent:'center' }}>
+                <Card sx={{ maxWidth: 390 }} style={{ marginBlock: 20, backgroundColor: 'gray', color: 'maroon', position: 'relative' }}>
+                    <CardHeader
+                        avatar={
+                            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                                <img className="alcoholImg" src={prop.data.imgUrl} />
+                            </Avatar>
+                        }
+                        action={
+                            <IconButton aria-label="settings">
+                                <MoreVertIcon />
+                            </IconButton>
+                        }
+                        title={prop.data.name}
+                        subheader={prop.data.category}
+                    />
+                    <CardMedia
+                        component="img"
+                        height="400"
+                        image={prop.data.imgUrl}
+                        alt="Paella dish"
+                        onClick={onclick}
+                    />
+                    <CardContent>
+                        <Typography variant="body2" color="text.secondary">
+
+                        </Typography>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                        <IconButton aria-label="add to favorites" onClick={like}>
+                            <FavoriteIcon />
+                        </IconButton>
+                        {/*<IconButton aria-label="share">
                         <ShareIcon />
                     </IconButton>*/}
-                    <ExpandMore
-                    expand={expanded}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
-                    aria-label="show more"
-                    >
-                    <ExpandMoreIcon />
-                    </ExpandMore>
-                </CardActions>
-                <Collapse in={expanded} timeout="auto" unmountOnExit>
-                    <CardContent>
-                    <Typography paragraph>{prop.data.name}</Typography>
-                    <Typography paragraph>
-                        종류 : {prop.data.category}
-                    </Typography>
-                    <Typography paragraph>
-                        당도 : {prop.data.sugar} (달수록 숫자가 높습니다)
-                    </Typography>
-                    <Typography paragraph>
-                        색 : {color}
-                    </Typography>
-                    <Typography paragraph>
-                        도수 : {prop.data.dosu}
-                    </Typography>
-                    <Typography paragraph>
-                        가격 : {prop.data.price}원 (판매점마다 가격이 다를 수 있습니다)
-                    </Typography>
-                    <Typography onClick={cocktail}>
-                        <strong>{prop.data.name}을 이용한 레시피 보기</strong>
-                    </Typography>
-                    </CardContent>
-                </Collapse>
+                        <ExpandMore
+                            expand={expanded}
+                            onClick={handleExpandClick}
+                            aria-expanded={expanded}
+                            aria-label="show more"
+                        >
+                            <ExpandMoreIcon />
+                        </ExpandMore>
+                    </CardActions>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        <CardContent>
+                            <Typography paragraph>{prop.data.name}</Typography>
+                            <Typography paragraph>
+                                종류 : {prop.data.category}
+                            </Typography>
+                            <Typography paragraph>
+                                당도 : {prop.data.sugar} (달수록 숫자가 높습니다)
+                            </Typography>
+                            <Typography paragraph>
+                                색 : {color}
+                            </Typography>
+                            <Typography paragraph>
+                                도수 : {prop.data.dosu}
+                            </Typography>
+                            <Typography paragraph>
+                                가격 : {prop.data.price}원 (판매점마다 가격이 다를 수 있습니다)
+                            </Typography>
+                            <Typography onClick={cocktail}>
+                                <strong>{prop.data.name}을 이용한 레시피 보기</strong>
+                            </Typography>
+                        </CardContent>
+                    </Collapse>
                 </Card>
+            </Grid>
             {/*<h2 className="board-title" >{}</h2>
             <span className="board-title">종류 : {}</span>
             <img className="alcoholImg" src={prop.data.imgUrl}/>*/}
-            
+
         </div>
     )
 }
