1d0121d8f0f8a335d044803313dba0b79b9a023d
chobkyu <qudqud97@naver.com>
2022-12-30 23:52:08 +0900

    cock

---
+++ alcohol/src/interface/cocktail/Cock.ts
@@ -0,0 +1,9 @@
+import { Cocktail } from "../Cocktail";
+import { CockAlcho } from "./CockAlcho";
+import { CockJuice } from "./CockJuice";
+
+export interface Cock{
+    cocktail:Cocktail;
+    cockAlcho:CockAlcho[];
+    cockJuice:CockJuice[];
+}
