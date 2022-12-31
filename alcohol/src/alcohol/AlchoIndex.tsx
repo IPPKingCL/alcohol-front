906256b3aa89a27a3ce8e2f99de2a94e1d7e8490
chobkyu <qudqud97@naver.com>
2022-12-31 17:40:05 +0900

    일단은

--- alcohol/src/alcohol/AlchoIndex.tsx
+++ alcohol/src/alcohol/AlchoIndex.tsx
@@ -14,8 +14,15 @@ const AlchoIndex = () =>{
             <h2>술 정보</h2>
             <span>다양한 술 정보와 칵테일 레시피</span>
             <hr></hr>
-            <img onClick={info} src="https://myhsproject.s3.ap-northeast-2.amazonaws.com/KakaoTalk_20221231_173124605.jpg"/>
-            <img onClick={recipe} src="https://myhsproject.s3.ap-northeast-2.amazonaws.com/istockphoto-1302161390-612x612.jpg"/>
+            <div>
+                <img onClick={info} src="https://myhsproject.s3.ap-northeast-2.amazonaws.com/KakaoTalk_20221231_173124605.jpg"/>
+                <strong>다양한 술을 알아보자</strong>
+            </div>
+            <hr></hr>
+            <div>
+                <img onClick={recipe} src="https://myhsproject.s3.ap-northeast-2.amazonaws.com/istockphoto-1302161390-612x612.jpg"/>
+                <strong>홈텐딩을 위한 칵테일 레시피</strong>
+            </div>
             
         </div>
     )
