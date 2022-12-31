51b35606d2d4b860ac864ae479a479497f03b3e4
chobkyu <qudqud97@naver.com>
2022-12-31 17:36:01 +0900

    약간 이런 느낌을 내고 싶은데 말이죠...

--- alcohol/src/alcohol/AlchoIndex.tsx
+++ alcohol/src/alcohol/AlchoIndex.tsx
@@ -12,6 +12,8 @@ const AlchoIndex = () =>{
     return (
         <div>
             <button onClick={info}>술정보</button>
+            <img src="https://myhsproject.s3.ap-northeast-2.amazonaws.com/KakaoTalk_20221231_173124605.jpg"/>
+            <img src="https://myhsproject.s3.ap-northeast-2.amazonaws.com/istockphoto-1302161390-612x612.jpg"/>
             <button onClick={recipe}>레시피</button>
         </div>
     )
