76ff57f1cb1079fdf2d078a91587a22da57b365e
chobkyu <qudqud97@naver.com>
2022-12-31 17:38:02 +0900

    Update AlchoIndex.tsx

--- alcohol/src/alcohol/AlchoIndex.tsx
+++ alcohol/src/alcohol/AlchoIndex.tsx
@@ -11,10 +11,12 @@ const AlchoIndex = () =>{
     }
     return (
         <div>
-            <button onClick={info}>술정보</button>
-            <img src="https://myhsproject.s3.ap-northeast-2.amazonaws.com/KakaoTalk_20221231_173124605.jpg"/>
-            <img src="https://myhsproject.s3.ap-northeast-2.amazonaws.com/istockphoto-1302161390-612x612.jpg"/>
-            <button onClick={recipe}>레시피</button>
+            <h2>술 정보</h2>
+            <span>다양한 술 정보와 칵테일 레시피</span>
+            <hr></hr>
+            <img onClick={info} src="https://myhsproject.s3.ap-northeast-2.amazonaws.com/KakaoTalk_20221231_173124605.jpg"/>
+            <img onClick={recipe} src="https://myhsproject.s3.ap-northeast-2.amazonaws.com/istockphoto-1302161390-612x612.jpg"/>
+            
         </div>
     )
 }
