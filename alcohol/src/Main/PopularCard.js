be0fecf76c6abc654cba3b8997bd49d4cd21e0e9
hjkwon <hjkwon0814@naver.com>
2023-01-01 23:33:38 +0900

    차트 제외

--- alcohol/src/Main/PopularCard.js
+++ alcohol/src/Main/PopularCard.js
@@ -43,7 +43,7 @@ const PopularCard = ({ isLoading }) => {
                             <Grid item xs={12}>
                                 <Grid container alignContent="center" justifyContent="space-between">
                                     <Grid item>
-                                        <Typography variant="h4">Popular Stocks</Typography>
+                                        <Typography variant="h4">인기 게시글</Typography>
                                     </Grid>
                                     <Grid item>
                                         <MoreHorizOutlinedIcon
@@ -78,9 +78,6 @@ const PopularCard = ({ isLoading }) => {
                                         </Menu>
                                     </Grid>
                                 </Grid>
-                            </Grid>
-                            <Grid item xs={12} sx={{ pt: '16px !important' }}>
-                                <BajajAreaChartCard />
                             </Grid>
                             <Grid item xs={12}>
                                 <Grid container direction="column">
