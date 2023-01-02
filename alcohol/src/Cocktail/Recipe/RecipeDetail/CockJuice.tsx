52d21f468fb2c149b4a4fe562e38f596f4dce56d
chobkyu <qudqud97@naver.com>
2023-01-02 10:16:21 +0900

    음료 띄우기

--- alcohol/src/Cocktail/Recipe/RecipeDetail/CockJuice.tsx
+++ alcohol/src/Cocktail/Recipe/RecipeDetail/CockJuice.tsx
@@ -1,14 +1,18 @@
 import CockJuiceOne from "./CockJuiceOne";
 
 const CockJuice = (props:any) => {
+    console.log(props)
     return (
         <div>
+            음료레시피
              {props.datas&&props.datas.map((data:any)=>(
                 <CockJuiceOne
                     data={data}
                     
                 />
+               
             ))}
+             <hr></hr>
         </div>
     )
 }
--- alcohol/src/Cocktail/Recipe/RecipeDetail/CockJuiceOne.tsx
+++ alcohol/src/Cocktail/Recipe/RecipeDetail/CockJuiceOne.tsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
+import { CockJuice } from "../../../interface/cocktail/CockJuice";
+
 const CockJuiceOne = (prop:any) => {
+    console.log(prop.data);
+    const [juice,setJuice] = useState<CockJuice>()
+    
+    
+    
+    useEffect(() => {
+        setJuice(prop.data)
+    },[]);
+
     return (
         <div>
-            {prop}
+            <img src={juice?.imgUrl}/>
+            {juice?.name}을(를) {juice?.amount}만큼 따라주세요
         </div>
     )
 }
