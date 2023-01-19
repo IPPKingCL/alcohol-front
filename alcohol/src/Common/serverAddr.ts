31b5653edc48f62c7a7805c3296d978d48ff7df0
chobkyu <qudqud97@naver.com>
2023-01-19 09:56:25 +0900

    Update serverAddr.ts

--- alcohol/src/Common/serverAddr.ts
+++ alcohol/src/Common/serverAddr.ts
@@ -1,7 +1,7 @@
-export const addr = "http://localhost:5000"
+//export const addr = "http://localhost:5000"
   
 
-// export const addr = "http://192.168.0.29:5000"
+export const addr = "http://192.168.0.29:5000"
 
 // export const addr = "http://192.168.0.30:5000"
 
