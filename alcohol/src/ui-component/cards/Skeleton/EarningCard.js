6c45aabc4367259a8d3436d40bec30cf178fa906
hjkwon0814 <hjkwon0814@naver.com>
2023-02-03 09:36:01 +0900

    오류수정

--- alcohol/src/ui-component/cards/Skeleton/EarningCard.js
+++ alcohol/src/ui-component/cards/Skeleton/EarningCard.js
@@ -1,12 +1,50 @@
 // material-ui
-import { Card, CardContent, Grid } from '@mui/material';
+import { styled, Box, Card, CardContent, Grid } from '@mui/material';
 import Skeleton from '@mui/material/Skeleton';
+import MainCard from '../../cards/MainCard';
 
 // ==============================|| SKELETON - EARNING CARD ||============================== //
 
-const EarningCard = () => (
+const CardWrapper = styled(MainCard)(({ theme }) => ({
+    backgroundColor: theme.palette.secondary.dark,
+    color: '#fff',
+    overflow: 'hidden',
+    position: 'relative',
+    '&:after': {
+        content: '""',
+        position: 'absolute',
+        width: 210,
+        height: 210,
+        background: theme.palette.secondary[800],
+        borderRadius: '50%',
+        top: -85,
+        right: -95,
+        [theme.breakpoints.down('sm')]: {
+            top: -105,
+            right: -140
+        }
+    },
+    '&:before': {
+        content: '""',
+        position: 'absolute',
+        width: 210,
+        height: 210,
+        background: theme.palette.secondary[800],
+        borderRadius: '50%',
+        top: -125,
+        right: -15,
+        opacity: 0.5,
+        [theme.breakpoints.down('sm')]: {
+            top: -155,
+            right: -70
+        }
+    }
+}));
 
-    <CardWrapper border={false} content={false} onClick={() => { console.log(image) }}>
+const EarningCard = () => (
+    
+
+    <CardWrapper border={false} content={false}>
         <Box sx={{ p: 2.25 }}>
             <Skeleton variant="rectangular" width={44} height={44} />
         </Box>
