85cf28a5a3399e7cc25b5ba9b7d41e5e6a172f5f
chobkyu <qudqud97@naver.com>
2023-02-09 16:31:16 +0900

    add + button

--- alcohol/src/Cocktail/SelfRecipe/SelfCock.tsx
+++ alcohol/src/Cocktail/SelfRecipe/SelfCock.tsx
@@ -1,103 +1,120 @@
+import Fab from "@mui/material/Fab";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import AlcoholSearch from "../../alcohol/AlcoholSearch/AlcoholSearch";
+import BasicSpeedDial from "../../Board/BasicSpeedDial";
 import { addr } from "../../Common/serverAddr";
 import { Cocktail } from "../../interface/Cocktail";
 import { RecipeList } from "../../interface/RecipeList";
 import RecipeListco from "../Recipe/RecipeListco";
+import AddIcon from "./AddIcon";
+
+
 
 const SelfCock = () => {
-    const [recipeList,setRecipeList] = useState<RecipeList[]>([]);
-    const [loading,setLoading] = useState<boolean>(true);
+    const [recipeList, setRecipeList] = useState<RecipeList[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const navigate = useNavigate();
 
     const list = async () => {
         setLoading(true);
         setRecipeList([]);
-        fetch(addr+'/selfcocktail',{
-            method:"Get",
-            headers:{
-                "Content-Type":"application/json",
-            },
-        }).then((res)=>res.json())
-        .then((res)=>{
-            setRecipeList(res);
-        })
-        setLoading(false);
-    }
-
-    useEffect(()=>{
-        list();
-    },[]);
-
-    const [search, setSearch] = useState<string>('');
-    const [searchData,setSearchData] = useState<Cocktail[]>([]);
-
-    //아직 검색 기능 없음    
-    const onChange = (e:any) => {
-        let category = '';
-        
-        category = e.target.value;
-        console.log('category : '+category);
-        setLoading(true);
-        setRecipeList([]);
-        fetch(addr+'/cocktail/search/'+category,{
-            method:'GET',
+        fetch(addr + '/selfcocktail', {
+            method: "Get",
             headers: {
                 "Content-Type": "application/json",
             },
         }).then((res) => res.json())
-        .then((res) => {
-            
-            let i:number = 0;
-            for (i; i < res.length; i++) {
-                const data:Cocktail ={
-                    id: res[i].id,
-                    name: res[i].name,
-                    dosu: res[i].dosu,
-                    imgUrl: res[i].imgUrl,
-                    only: res[i].only,
-                    likeOne: res[i].likeOne
-                }
-                setRecipeList(recipeList => [...recipeList,data]);
-                
-            }
-            
-            setLoading(false);
-        })
+            .then((res) => {
+                setRecipeList(res);
+            })
+        setLoading(false);
     }
-    
-    const onSearch = (e:React.FormEvent<HTMLFormElement>) => {
-        
+
+    useEffect(() => {
+        list();
+    }, []);
+
+    const [search, setSearch] = useState<string>('');
+    const [searchData, setSearchData] = useState<Cocktail[]>([]);
+
+    //아직 검색 기능 없음    
+    const onChange = (e: any) => {
+        let category = '';
+
+        category = e.target.value;
+        console.log('category : ' + category);
+        setLoading(true);
+        setRecipeList([]);
+        fetch(addr + '/cocktail/search/' + category, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }).then((res) => res.json())
+            .then((res) => {
+
+                let i: number = 0;
+                for (i; i < res.length; i++) {
+                    const data: Cocktail = {
+                        id: res[i].id,
+                        name: res[i].name,
+                        dosu: res[i].dosu,
+                        imgUrl: res[i].imgUrl,
+                        only: res[i].only,
+                        likeOne: res[i].likeOne
+                    }
+                    setRecipeList(recipeList => [...recipeList, data]);
+
+                }
+
+                setLoading(false);
+            })
+    }
+
+    const onSearch = (e: React.FormEvent<HTMLFormElement>) => {
+
         e.preventDefault();
         console.log(search);
-        if(search==''){
+        if (search == '') {
             list();
             return;
         }
-        const filterData = recipeList.filter((row)=>row.name.includes(search));
+        const filterData = recipeList.filter((row) => row.name.includes(search));
 
         setRecipeList(filterData)
         setSearch('');
     }
 
-    const onChangeSearch = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
         e.preventDefault();
         setSearch(e.target.value);
     }
 
-    return(
+
+    const insert = () => {
+        navigate('/cocktail/recipe/self/insert');
+    }
+    return (
         <div id='wrapper2'>
             <AlcoholSearch
-                 onSearch={onSearch}
-                 onChange={onChange}
-                 onChangeSearch = {onChangeSearch}
+                onSearch={onSearch}
+                onChange={onChange}
+                onChangeSearch={onChangeSearch}
             />
 
             <hr></hr>
-            {loading ? <strong>loading....</strong>:
-                
-               <p>hiyo</p>
+            {loading ? <strong>loading....</strong> :
+
+                <RecipeListco
+                    datas={recipeList} />
             }
-            
+
+            <AddIcon click={insert}/>
+
+
+
+
         </div>
     )
 }
