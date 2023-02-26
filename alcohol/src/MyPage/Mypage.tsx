2f0f3a26f7a3ae0ab102f7f182e25ca4d62106a7
hjkwon <hjkwon0814@naver.com>
2023-02-26 22:57:51 +0900

    mypage 리스트

--- alcohol/src/MyPage/Mypage.tsx
+++ alcohol/src/MyPage/Mypage.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Paper, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+import { Avatar, Button, CardContent, Paper, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
 import { ReactNode, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import { Card, CardHeader } from '@mui/material';
@@ -14,17 +14,35 @@ function MyPage() {
     const navigate = useNavigate();
 
     return (
-        <Card style={{ backgroundColor: '#FFFFB6', color: 'maroon', position: 'relative' }}>
-            <CardHeader
+        <>
+            <Card style={{ backgroundColor: '#FFFFB6', color: 'maroon', position: 'relative' }}>
+                <CardHeader
                     avatar={
                         <Avatar src="" aria-label="recipe" />
                     }
                     title={<Typography>
                         <span style={{ fontSize: "18px" }}>마이 페이지</span>
                     </Typography>}
-                    subheader={<Typography><span onClick={() => {navigate('/mypageInfo')}}>나의 정보 보러가기</span></Typography>}
+                    subheader={<Typography><span onClick={() => { navigate('/mypageInfo') }}>나의 정보 보러가기</span></Typography>}
                 />
-        </Card>
+            </Card>
+            <Card style={{ backgroundColor: '#FFFFB6', color: 'maroon', position: 'relative' , marginTop: '1rem'}}>
+                <CardContent>
+                    <Typography variant="h6" color="text.secondary" sx={{
+                    }}>
+                       내가 좋아하는 칵테일
+                    </Typography>
+                </CardContent>
+            </Card>
+            <Card style={{ backgroundColor: '#FFFFB6', color: 'maroon', position: 'relative'}}>
+                <CardContent>
+                    <Typography variant="h6" color="text.secondary" sx={{
+                    }}>
+                       내가 좋아하는 술
+                    </Typography>
+                </CardContent>
+            </Card>
+        </>
     )
 }
 
